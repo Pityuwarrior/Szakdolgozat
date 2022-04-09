c9f53ada42381d53e3476570cd28b107747c56e9
Pityuwarrior <nove.norbert2002@gmail.com>
2022-04-09 22:01:50 +0200

    2022.04.09

--- Codes/front/pages/programming.php
+++ Codes/front/pages/programming.php
@@ -1,12 +1,19 @@
-<section class = "flex_programming">
-    <div class = "flex_progtext">
-        <p>urfhsdehfjosdfhoupsd</p>
-    </div>  
+<section class = "flex_programming_c">
     <div class = "flex_inner">
-        <div class = "pic_item"><img src = "./front/Resources/img/p_aboutme.png" alt = "me"></div>
-        <div class = "text_item">
-             <h3>Rólam szöveg</h3>
-             <p>###I am a professional zegz-er, You might ask what does that mean, well it does not matter what it does matter is that I can do it professionally so lick my boots please. While you're there look at my portfolio you won't regret it you'll find a talented man's art and that would be me.###</p>
-         </div>
+            <div class = "pic_item"><a href="https://github.com/Pityuwarrior/Tojasgyar_NNI" target="_blank"><img src = "./front/Resources/img/C.png" title = "Repository"></a></div>
+            <div class = "flex_progtext">
+                <h3>C#</h3>
+                <p>###I am a professional zegz-er, You might ask what does that mean, well it does not matter what it does matter is that I can do it professionally so lick my boots please. While you're there look at my portfolio you won't regret it you'll find a talented man's art and that would be me.###</p>
+            </div>
+        </div>       
     </div>
+</section>
+<section class = "flex_programming_java">
+<div class = "flex_inner">
+            <div class = "pic_item"><a href="https://github.com/Pityuwarrior/Java_car_project" target="_blank"><img src = "./front/Resources/img/Java.png" title = "Repository"></a></div>
+            <div class = "flex_progtext">
+                <h3>Java</h3>
+                <p>###I am a professional zegz-er, You might ask what does that mean, well it does not matter what it does matter is that I can do it professionally so lick my boots please. While you're there look at my portfolio you won't regret it you'll find a talented man's art and that would be me.###</p>
+            </div>     
+        </div>        
 </section>
